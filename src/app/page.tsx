655aa7995bae4b1b2f97983b790aa2f72f24410a
kinako1415 <Owata09@outlook.jp>
2024-09-27 04:37:19 +0900

    レイアウト調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Button, Container, Dialog, Flex, Text } from "@radix-ui/themes";
+import { Button, Dialog, Flex, Text } from "@radix-ui/themes";
 import "./page.scss";
 import { useEffect, useState } from "react";
 import Count from "@/src/components/count";
@@ -116,191 +116,189 @@ export default function Index(): JSX.Element {
     });
 
     return (
-        <Box mt="2rem" width="100%">
-            <Text ml="6vw" size="6" weight="bold">
+        <Flex align="center" direction="column" mt="2rem" width="100%">
+            <Text ml="3vw" size="8" weight="bold">
                 モバイル注文
             </Text>
-            <Container size="4">
-                <Flex align="center" gap="4" justify="center" m="2rem">
-                    <Dialog.Root>
-                        <Dialog.Trigger>
-                            <img alt="furannku" src="/furanku.png" />
-                        </Dialog.Trigger>
-                        <Dialog.Content>
-                            <Flex direction="column" gap="4" width="100%">
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        ケチャップ
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="frankfurt"
-                                        taste="ketchupCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        マスタード
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="frankfurt"
-                                        taste="mustardCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        ケチャマスタード
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="frankfurt"
-                                        taste="ketchupMustardCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        塩コショウ
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="frankfurt"
-                                        taste="saltAndPepperCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        トッピングなし
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="frankfurt"
-                                        taste="normalCount"
-                                    />
-                                </Flex>
+            <Flex align="center" gap="4" justify="center" m="1rem" mt="2rem">
+                <Dialog.Root>
+                    <Dialog.Trigger>
+                        <img alt="furannku" src="/furanku.png" />
+                    </Dialog.Trigger>
+                    <Dialog.Content>
+                        <Flex direction="column" gap="4" width="100%">
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    ケチャップ
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="frankfurt"
+                                    taste="ketchupCount"
+                                />
                             </Flex>
-                            <Dialog.Close>
-                                <Flex justify="end">
-                                    <Button color="gray" mt="1rem" variant="soft">
-                                        Close
-                                    </Button>
-                                </Flex>
-                            </Dialog.Close>
-                        </Dialog.Content>
-                    </Dialog.Root>
-                    <Flex align="center" className="number-card" justify="center" pl="5px" pr="5px">
-                        {frankfurtTotal}
-                    </Flex>
-                    <Text size="5" weight="bold">
-                        本
-                    </Text>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    マスタード
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="frankfurt"
+                                    taste="mustardCount"
+                                />
+                            </Flex>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    ケチャマスタード
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="frankfurt"
+                                    taste="ketchupMustardCount"
+                                />
+                            </Flex>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    塩コショウ
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="frankfurt"
+                                    taste="saltAndPepperCount"
+                                />
+                            </Flex>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    トッピングなし
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="frankfurt"
+                                    taste="normalCount"
+                                />
+                            </Flex>
+                        </Flex>
+                        <Dialog.Close>
+                            <Flex justify="end">
+                                <Button color="gray" mt="1rem" variant="soft">
+                                    Close
+                                </Button>
+                            </Flex>
+                        </Dialog.Close>
+                    </Dialog.Content>
+                </Dialog.Root>
+                <Flex align="center" className="number-card" justify="center" pl="5px" pr="5px">
+                    {frankfurtTotal}
                 </Flex>
-
-                <Text ml="6vw" size="7" weight="bold">
-                    セットで300円
+                <Text size="5" weight="bold">
+                    本
                 </Text>
+            </Flex>
 
-                <Flex align="center" gap="4" justify="center" m="2rem">
-                    <Dialog.Root>
-                        <Dialog.Trigger>
-                            <img alt="furannku" src="/cheesefuranku.png" />
-                        </Dialog.Trigger>
-                        <Dialog.Content>
-                            <Flex direction="column" gap="4" width="100%">
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        ケチャップ
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="cheeseFrankfurt"
-                                        taste="ketchupCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        マスタード
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="cheeseFrankfurt"
-                                        taste="mustardCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        ケチャマスタード
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="cheeseFrankfurt"
-                                        taste="ketchupMustardCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        塩コショウ
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="cheeseFrankfurt"
-                                        taste="saltAndPepperCount"
-                                    />
-                                </Flex>
-                                <Flex justify="between">
-                                    <Text size="5" weight="bold">
-                                        トッピングなし
-                                    </Text>
-                                    <Count
-                                        decrementCount={decrementCount}
-                                        incrementCount={incrementCount}
-                                        order={values}
-                                        sausage="cheeseFrankfurt"
-                                        taste="normalCount"
-                                    />
-                                </Flex>
+            <Text ml="3vw" size="7" weight="bold">
+                セットで300円
+            </Text>
+
+            <Flex align="center" gap="4" justify="center" m="1rem">
+                <Dialog.Root>
+                    <Dialog.Trigger>
+                        <img alt="furannku" src="/cheesefuranku.png" />
+                    </Dialog.Trigger>
+                    <Dialog.Content>
+                        <Flex direction="column" gap="4" width="100%">
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    ケチャップ
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="cheeseFrankfurt"
+                                    taste="ketchupCount"
+                                />
                             </Flex>
-                            <Dialog.Close>
-                                <Flex justify="end">
-                                    <Button color="gray" mt="1rem" variant="soft">
-                                        Close
-                                    </Button>
-                                </Flex>
-                            </Dialog.Close>
-                        </Dialog.Content>
-                    </Dialog.Root>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    マスタード
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="cheeseFrankfurt"
+                                    taste="mustardCount"
+                                />
+                            </Flex>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    ケチャマスタード
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="cheeseFrankfurt"
+                                    taste="ketchupMustardCount"
+                                />
+                            </Flex>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    塩コショウ
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="cheeseFrankfurt"
+                                    taste="saltAndPepperCount"
+                                />
+                            </Flex>
+                            <Flex justify="between">
+                                <Text size="5" weight="bold">
+                                    トッピングなし
+                                </Text>
+                                <Count
+                                    decrementCount={decrementCount}
+                                    incrementCount={incrementCount}
+                                    order={values}
+                                    sausage="cheeseFrankfurt"
+                                    taste="normalCount"
+                                />
+                            </Flex>
+                        </Flex>
+                        <Dialog.Close>
+                            <Flex justify="end">
+                                <Button color="gray" mt="1rem" variant="soft">
+                                    Close
+                                </Button>
+                            </Flex>
+                        </Dialog.Close>
+                    </Dialog.Content>
+                </Dialog.Root>
 
-                    <Flex align="center" className="number-card" justify="center" pl="5px" pr="5px">
-                        {cheeseFrankfurtTotal}
-                    </Flex>
-
-                    <Text size="5" weight="bold">
-                        本
-                    </Text>
+                <Flex align="center" className="number-card" justify="center" pl="5px" pr="5px">
+                    {cheeseFrankfurtTotal}
                 </Flex>
-            </Container>
+
+                <Text size="5" weight="bold">
+                    本
+                </Text>
+            </Flex>
 
             <button aria-label="Next" id="next-button" onClick={handleNext} type="button">
                 <img alt="つぎへ" src="/nextend.png" />
             </button>
-        </Box>
+        </Flex>
     );
 }
